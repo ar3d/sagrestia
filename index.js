91c60c05b3e7a56fc850240f14c071080b563605
ar3d <info@ar3d.it>
2022-11-26 10:05:58 +0100

    ok

--- index.js
+++ index.js
@@ -16,32 +16,25 @@
 'use strict';
 
 // Create viewer.
-var viewer = new Marzipano.Viewer(document.getElementById('pano'));
+var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
 
-// Register the custom control method.
-var deviceOrientationControlMethod = new DeviceOrientationControlMethod();
-var controls = viewer.controls();
-controls.registerMethod('deviceOrientation', deviceOrientationControlMethod);
+  // Create scenes.
+  var scenes = data.scenes.map(function(data) {
+    var urlPrefix = "tiles";
+    var source = Marzipano.ImageUrlSource.fromString(
+      urlPrefix + "/" + data.id + "/{z}/{f}/{y}/{x}.jpg",
+      { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" });
+    var geometry = new Marzipano.CubeGeometry(data.levels);
 
-// Create source.
-var source = Marzipano.ImageUrlSource.fromString(
-  "//www.marzipano.net/media/cubemap/{f}.jpg"
-);
+    var limiter = Marzipano.RectilinearView.limit.traditional(data.faceSize, 100*Math.PI/180, 120*Math.PI/180);
+    var view = new Marzipano.RectilinearView(data.initialViewParameters, limiter);
 
-// Create geometry.
-var geometry = new Marzipano.CubeGeometry([{ tileSize: 1024, size: 1024 }]);
-
-// Create view.
-var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100 * Math.PI / 180);
-var view = new Marzipano.RectilinearView(null, limiter);
-
-// Create scene.
-var scene = viewer.createScene({
-  source: source,
-  geometry: geometry,
-  view: view,
-  pinFirstLevel: true
-});
+    var scene = viewer.createScene({
+      source: source,
+      geometry: geometry,
+      view: view,
+      pinFirstLevel: true
+    });
 
 // Display scene.
 scene.switchTo();
